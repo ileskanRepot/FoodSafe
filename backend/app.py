4105283b89163660117a834f232ed5b34eca96fc
Ileska <ileska@tutanota.com>
2023-11-04 16:17:56 +0200

    Sef flask return frontend files also (ditch react)

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
 from time import time
-from flask import Flask,jsonify,send_file
+from flask import Flask,jsonify,send_file,render_template,send_from_directory
 
 app = Flask(__name__)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def main():
+    return send_from_directory(".",path="leaflet.html")
+
+@app.route('/app.js')
+def appJs():
+    return send_from_directory(".",path="app.js")
 
 @app.route('/localMap')
 def localMap():
@@ -20,4 +24,9 @@ def worldMap():
 @app.route('/dummyGeo.json')
 def dummyGeo():
     return send_file("./Dummy_data.geojson")
+    # return send_file("./worldMap.json")
+
+@app.route('/vuln.csv')
+def vuln():
+    return send_file("./vuln.csv")
     # return send_file("./worldMap.json")
