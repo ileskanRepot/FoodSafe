41a06341918bc33d5816ede2ec5b8ed371a613e1
Ileska <ileska@tutanota.com>
2023-11-04 12:03:33 +0200

    Change url

--- frontend/src/pages/LocalMap.tsx
+++ frontend/src/pages/LocalMap.tsx
@@ -1,14 +1,36 @@
-import React /*useState */ from "react";
+import React, { useEffect, useState } from "react";
 // import { useCookies } from "react-cookie";
 // import { useNavigate } from "react-router-dom";
-// import settings from "../constants";
+import settings from "../constants";
 
-const Login = () => {
+const LocalMap = () => {
+  const [getDate, setDate] = useState<string>("");
+
+  useEffect(() => {
+    // let url = `${settings.backendUrl}/api/isLoggedIn`;
+    let url = `/api/localMap`;
+    console.log(url);
+    fetch(url, {
+      // credentials: "include",
+      // mode: "navigate",
+      // method: "get",
+      // crossorigin: "*",
+    })
+      .then((resp) => {
+        console.log(resp);
+        return resp.status;
+      })
+      .then((resp) => {
+        console.log(resp);
+        setDate(resp.toString());
+      });
+  }, []);
+
   return (
     <>
-      <h1>LocalMap</h1>
+      <h1>LocalMap {getDate}</h1>
     </>
   );
 };
 
-export default Login;
+export default LocalMap;
