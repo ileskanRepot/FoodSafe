f0a5992c2ba28ac8b3a541d1a4546092ba8a60b6
Ileska <ileska@tutanota.com>
2023-11-04 11:09:45 +0200

    Add flask-cors

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@
   pkgs.mkShell {
     nativeBuildInputs = let
       env = pyPkgs : with pyPkgs; [
+        autopep8
         flask
+        flask-cors
       ];
     in with pkgs; [
       (python311.withPackages env)
