9e9e23d55179b9ecf70a861d85d5071565ad916a
Ileska <ileska@tutanota.com>
2023-11-04 20:48:00 +0200

    Add legend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
+let startPos = [3.0, 31.166667];
 // Initialize the map
+let floodUrl = "https://ows.globalfloods.eu/glofas-ows/ows?";
+// let floodUrl = "http://ows.mundialis.de/services/service?";
 let url = "/dummyGeo.json";
 
 let nn = 0.0027;
@@ -24,7 +27,7 @@ let nn = 0.0027;
 // };
 
 // var map = L.map("map").setView([51.505, -0.09], 15);
-var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map("map").setView(startPos, 5);
 
 // Add the tile layer (you can choose a different map style by changing the URL)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -40,17 +43,52 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 //   color: "red",
 //   radius: 500, // Radius in meters
 // }).addTo(map);
+var wmsLayer = L.tileLayer
+  .wms(floodUrl, {
+    transparent: true,
+    format: "image/png",
+    layers: "FloodSummary1_30",
+  })
+  .addTo(map);
+
+var legend = L.control({ position: "bottomleft" });
+
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create("div", "legend");
+  div.style.backgroundColor = "#ff00ff50";
+  div.style.display = "flex";
+  div.style.flexDirection = "column";
+  div.style.padding = "5%";
+  div.innerHTML += "<h4>INFORMATION</h4>";
+  div.innerHTML += '<button onclick="removeLayer()">TOGGLE FLOODS</button>';
+
+  return div;
+};
+
+legend.addTo(map);
 
 // Add a marker with a popup
-var marker1 = L.marker([51.505, -0.09])
+var marker1 = L.marker(startPos)
   .addTo(map)
   .bindPopup("<b>Hello World!</b><br/> I am a popup.");
 
-fetch(url)
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((resp) => {
-    console.log(resp);
-    L.geoJSON(resp).addTo(map);
-  });
+let layerToggle = true;
+const removeLayer = () => {
+  if (layerToggle) {
+    map.removeLayer(wmsLayer);
+    layerToggle = false;
+  } else {
+    map.addLayer(wmsLayer);
+    layerToggle = true;
+  }
+};
+
+// console.log(map.setView());
+// fetch(url)
+//   .then((resp) => {
+//     return resp.json();
+//   })
+//   .then((resp) => {
+//     console.log(resp);
+//     L.geoJSON(resp).addTo(map);
+//   });
