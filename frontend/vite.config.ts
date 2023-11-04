5301bc64f277cb9e8400020b7e3bf81788acddae
Ileska <ileska@tutanota.com>
2023-11-04 12:03:42 +0200

    Add proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,18 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
+  server: {
+    port: 3000,
+    cors: true,
+    proxy: {
+      "/api": {
+      target: "http://127.0.0.1:5000/",
+      changeOrigin: true,
+      secure: false,
+      rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 	// build: {
 		// outDir: "build"
 	// }
